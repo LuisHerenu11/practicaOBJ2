2e226c8c83d32bc6e45a7d552c5a13d04d0ec7e3
LuisHerenu11 <ElCondor_1106@hotmail.com>
2020-04-10 20:25:29 -0300

    agregado git ignore

---
+++ src/main/java/ar/edu/unahur/Ejercicio1.java
@@ -0,0 +1,11 @@
+package ar.edu.unahur;
+
+public class Ejercicio1 {
+    public static void main(String [] args){
+        int v [] = {2,4,7,2,5,9,1,8,3,10};
+
+        for(int i=0 ; i < v.length; i++){
+            System.out.println("Arreglo en indice:"+i+" es igual a: " + v[i]);
+        }
+    }
+}

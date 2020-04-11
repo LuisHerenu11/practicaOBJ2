cb99783bbcfdcbcfda02a98b0fef46da4bff8ab2
LuisHerenu11 <ElCondor_1106@hotmail.com>
2020-04-11 01:41:54 -0300

    ejercicio 1 completo

--- src/main/java/ar/edu/unahur/Ejercicio1.java
+++ src/main/java/ar/edu/unahur/Ejercicio1.java
@@ -1,11 +1,33 @@
 package ar.edu.unahur;
 
+import java.util.Scanner;
+
 public class Ejercicio1 {
     public static void main(String [] args){
-        int v [] = {2,4,7,2,5,9,1,8,3,10};
 
-        for(int i=0 ; i < v.length; i++){
-            System.out.println("Arreglo en indice:"+i+" es igual a: " + v[i]);
+        Scanner lector = new Scanner(System.in);
+        int a,i,aux,b;
+        int array[] = new int[20];
+        System.out.print("Ingrese el tamaño del array ");
+        a = lector.nextInt();
+        for (i=0; i<a;i++) {
+            System.out.print("X["+(i + 1)+"]= ");
+            array[i]=lector.nextInt();
+        }
+        System.out.println("Array normal");
+        for(int j=0; j <a; j++){
+            System.out.print(array[j]+", ");
+        }
+        b = a;
+        for(i = 0;i<=b/2; i++){
+            aux= array[i];
+            array[i] = array[b - 1];
+            array[b-1] = aux;
+            b--;
+        }
+        System.out.println("array invertido");
+        for(i =0; i<a;i++){
+            System.out.print(array[i]+", ");
         }
     }
 }
